ea0732eebaac029922712b800324cd3af8b8769c
Yuehan Huang <yuehan.huang@protonmail.com>
2020-08-13 18:23:17 -0700

    W3D4

---
+++ W3D4/board.rb
@@ -0,0 +1,13 @@
+require_relative "tiles"
+class Board
+    def initialize(num_of_bombs)
+        @board = Array.new(9) {Array.new(9)}
+        bombs = []
+        num_of_bombs.times do
+            bombs << Tile.new(true)
+        end
+        bombs.shuffle
+        bombs.shuffle!
+        
+        # 81 - num_of_
+end
