801a30efac4d69d7d47c8b3d72cf6549fedda0f9
Yuehan Huang <yuehan.huang@protonmail.com>
2020-08-21 13:52:16 -0700

    Create two_sum method of different complexity

---
+++ W4/W4D5/two_sum_problem.rb
@@ -0,0 +1,49 @@
+def bad_two_sum?(arr, num)
+    arr.each_index do |i|
+        (i + 1 ...arr.length).each do |j|
+            return true if arr[i] + arr[j] == num
+        end
+    end
+    false
+end
+
+
+arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+
+def okay_two_sum?(arr, num)
+    arr1 = arr.sort
+    i = 0 
+    j = arr.length - 1
+
+    while i < j
+        case arr[i] + arr[j] <=> num
+        when -1
+            i += 1
+        when 0
+            return true
+        when 1
+            j -= 1
+        end
+    end
+    false
+end
+
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
+
+target
+array
+hash
+
+
+def two_sum?(arr,num) #n
+    hsh = {}
+    arr.each do |el|
+        return true if hsh.key?{el}
+        
+        hsh[num - el] = true
+    end
+    false
+end
