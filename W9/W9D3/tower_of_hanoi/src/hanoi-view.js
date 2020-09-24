3e721215ab0b5ade82419019ba08762c4d5b9e31
Yuehan <yuehan.huang@protonmail.com>
2020-09-23 18:29:51 -0700

    Able to move discs on frontend

---
+++ W9/W9D3/tower_of_hanoi/src/hanoi-view.js
@@ -0,0 +1,59 @@
+class HanoiView {
+    constructor(game, rootEl) {
+        this.game = game;
+        this.root = rootEl;
+        this.setupTowers();
+        this.render();
+        this.ivar;
+        this.clickTower();
+    }
+
+    setupTowers() {
+        // const $piles = $("<ul></ul>").addClass("piles");
+        // this.root.append($piles);
+        for (let i = 0; i < 3; i++) {
+            let $pile = $("<ul></ul>")
+                .addClass(`pile-${i} pile`)
+                .data({ idx: i });
+            this.root.append($pile);
+        }
+    }
+
+    render() {
+        $(".disc").remove();
+        this.game.towers.forEach((pile, pileIdx) =>
+            pile
+                .slice()
+                .reverse()
+                .forEach((disc) => {
+                    let $disc = $("<li></li>")
+                        .addClass(`disc-${disc} disc`)
+                        .data({ idx: disc });
+                    $(`.pile-${pileIdx}`).append($disc);
+                })
+        );
+    }
+
+    clickTower() {
+        const that = this;
+        this.root.on("click", "ul", function (event) {
+            if (that.ivar == undefined) {
+                // need == comparison here since 0 is one of the value
+                that.ivar = $(event.currentTarget).data().idx;
+            } else {
+                let response = that.game.move(
+                    that.ivar,
+                    $(event.currentTarget).data().idx
+                );
+                if (response) {
+                    that.render();
+                } else {
+                    alert("False move");
+                }
+                that.ivar = undefined;
+            }
+        });
+    }
+}
+
+module.exports = HanoiView;
--- W9/W9D3/tower_of_hanoi/src/index.js
+++ W9/W9D3/tower_of_hanoi/src/index.js
@@ -1,5 +1,5 @@
 const HanoiGame = require('./game.js')
-
+const HanoiView = require('./hanoi-view.js')
 $(() => {
   const rootEl = $('.hanoi');
   const game = new HanoiGame();
