16cfef65399aa12eb165062c1b63ea9592b15912
Yuehan <yuehan.huang@protonmail.com>
2020-10-01 17:42:02 -0700

    Create Todo Proejct

---
+++ W10/W10D4/todos/todo_redux.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function(){
+    ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'))
+})
