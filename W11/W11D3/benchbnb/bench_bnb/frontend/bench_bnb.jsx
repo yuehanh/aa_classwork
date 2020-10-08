fb7e50731ab8956bdbc7507344f7c8ada05ce16e
Yuehan <yuehan.huang@protonmail.com>
2020-10-07 17:02:18 -0700

    Complete auth reducers and  auth

--- W11/W11D3/benchbnb/bench_bnb/frontend/bench_bnb.jsx
+++ W11/W11D3/benchbnb/bench_bnb/frontend/bench_bnb.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+
+// test import
+import * as SessionUtil from "./util/session_api_util";
+import { login, logout, signUp } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    let preloadedState = undefined;
+    const store = configureStore(preloadedState);
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+    // Test Section
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.signUp = signUp;
+    window.login = login;
+    window.logout = logout;
 });
